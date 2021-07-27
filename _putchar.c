61f94b954f9f573b7f74267c29ed90e73cf42f74
Kiki-100 <nyaga6178@gmail.com>
2021-07-27 06:55:39 -0700

    putchar files

--- _putchar.c
+++ _putchar.c
@@ -1,38 +1,34 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include "holberton.h"
 
 /**
- * _puts - prints a string with newline
- * @str: the string to print
- *
- * Return: void
+ * struct modifiers - Defines a specifier to data types
+ * @mod: type of data type
+ * @func_mod: pointer to function that prints data types
+ * according to their notation
  */
-int _puts(char *str)
+typedef struct modifiers
 {
-char *a = str;
+char *mod;
+int (*func_mod)(va_list argptr);
+} mod_t;
 
-while (*str)
-_putchar(*str++);
-return (str - a);
-}
+/* printf.c prototypes */
+int _printf(const char *format, ...);
+int print_modifiers(const char *format, va_list argptr, mod_t *fmt_list);
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(int c)
-{
-static int i;
-static char buf[OUTPUT_BUF_SIZE];
+/* specifier_funcs.c prototypes */
+int print_char(va_list argptr);
+int print_digit(va_list argptr);
+int print_string(va_list argptr);
+int print_rot13(va_list argptr);
 
-if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
-{
-write(1, buf, i);
-i = 0;
-}
-if (c != BUF_FLUSH)
-buf[i++] = c;
-return (1);
-}
+/* f_putchar.c prototype */
+int f_putchar(char c);
+
+#endif /* HOLBERTON_H */
