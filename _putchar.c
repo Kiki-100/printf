958da30889b5eaf134e4353d26e48844a8c1ff37
Kiki-100 <nyaga6178@gmail.com>
2021-07-27 07:18:01 -0700

    files

--- _putchar.c
+++ _putchar.c
@@ -1,34 +1,21 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-#include <stdarg.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include "holberton.h"
 
 /**
- * struct modifiers - Defines a specifier to data types
- * @mod: type of data type
- * @func_mod: pointer to function that prints data types
- * according to their notation
+ * f_putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ *
+ * Desc: First parameter in the write function is the file
+ * descriptor of where to
+ * write the output, we use one for standard output
+ * Second parameter is a null terminated character string of the
+ * content to write. We give the address to our char c parameter
+ * Last parameter is the number of bytes to write. We have one character
+ * we want to write that is 1 byte so we put a 1 as the parameter
  */
-typedef struct modifiers
+int f_putchar(char c)
 {
-char *mod;
-int (*func_mod)(va_list argptr);
-} mod_t;
-
-/* printf.c prototypes */
-int _printf(const char *format, ...);
-int print_modifiers(const char *format, va_list argptr, mod_t *fmt_list);
-
-/* specifier_funcs.c prototypes */
-int print_char(va_list argptr);
-int print_digit(va_list argptr);
-int print_string(va_list argptr);
-int print_rot13(va_list argptr);
-
-/* f_putchar.c prototype */
-int f_putchar(char c);
-
-#endif /* HOLBERTON_H */
+return (write(1, &c, 1));
+}
