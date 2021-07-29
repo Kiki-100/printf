6e78b862e8e08d0e13a8118cddde45e3ac208162
George Okumu <gokumu12@gmail.com>
2021-07-29 17:38:32 +0300

    new changes failing, reverted to old changes

--- _printf.c
+++ _printf.c
@@ -1,52 +1,24 @@
-#include <stdlib.h>
-#include <stdarg.h>
 #include "holberton.h"
 /**
- * check_specifier - checks valid specifier format
- * @format: any format specifier found when itaration occurs
- * Return: pointer to null pointer
- */
-static int (*check_specifier(const char *format))(va_list)
-{
-unsigned int i;
-/*w[] holds the array of the specifiers*/
-print_mod w[] = {
-{"cha", print_char},
-{"str", print_string},
-{"digit", print_digit},
-{"d", print_d},
-{"unsign", print_unsigned},
-{"b", print_b},
-{"octa", print_octal},
-{"hexa", print_hexadecimal},
-{"X", print_X},
-{"p", print_adress},
-{"S", print_S},
-{"r", print_r},
-{"R", print_R},
-{NULL, NULL}
-};
-/*Iteration*/
-for (i = 0; w[i].mod != NULL; i++)
-{
-if (*(w[i].mod) == *format)
-{
-break;
-}
-}
-return (w[i].func_mod);
-}
-/**
- * _printf - function that prints anything
- * @format: list of types of arguments passed to the function
- * @...: number of arguments
- * Return: modifiers
+ * _printf - receives all necessary arguments
+ * @format: String containing all arguments
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-unsigned int i = 0;
-unsigned int count = 0;
 va_list args;
+int mods;
+
+mod_t  fmt_list[] = {
+{"c", print_char},
+{"i", print_digit},
+{"d", print_digit},
+{"s", print_string},
+{"R", print_rot13},
+{NULL, NULL}
+};
+
+va_start(args, format);
 int (*func_mod)(va_list);
 
 if (format == NULL)
@@ -54,34 +26,7 @@ if (format == NULL)
 return (-1);
 }
 
-va_start(args, format);
-while (format[i])
-{
-
-for (; format[i] != '%' && format[i]; i++)
-{
-_putchar(format[i]);
-count++;
-}
-
-if (!format[i])
-return (count);
-func_mod = check_specifier(&format[i + 1]);
-if (func_mod != NULL)
-{
-count += func_mod(args);
-i += 2;
-continue;
-}
-if (!format[i + 1])
-return (-1);
-_putchar(format[i]);
-count++;
-if (format[i + 1] == '%')
-i += 2;
-else
-i++;
-}
+mods = print_modifiers(format, args, fmt_list);
 va_end(args);
-return (count);
+return (mods);
 }
--- holberton.h
+++ holberton.h
@@ -8,38 +8,29 @@
 #include <limits.h>
 
 /**
- * struct print - structure for printing various types
- * @t: type to print
- * @f: function to print
+ * struct modifiers - Defines a specifier to data types
+ * @mod: type of data type
+ * @func_mod: pointer to function that prints data types
+ * according to their notation
  */
-typedef struct print
+typedef struct modifiers
 {
-	char *mod;
-	int (*func_mod)(va_list);
-} print_mod;
+char *mod;
+int (*func_mod)(va_list argptr);
+} mod_t;
 
 /* _printf.c prototypes */
 int _printf(const char *format, ...);
-/*int print_modifiers(const char *format, va_list argptr, mod_t *fmt_list);*/
+int print_modifiers(const char *format, va_list argptr, mod_t *fmt_list);
 
 /* mod_funcs.c prototypes */
-int print_char(va_list cha);
-int print_string(va_list str);
-int print_digit(va_list digit);
-int print_d(va_list d);
-int print_unsigned(va_list unsign);
-int print_b(va_list b);
-int print_octal(va_list octa);
-int print_hexadecimal(va_list hexa);/*prints in lower case*/
-int print_X(va_list X); /*Prints alphabetical orders in caps*/
-int print_adress(va_list p);/*used to print the address in a hexadecimal form.*/
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
+int print_char(va_list argptr);
+int print_digit(va_list argptr);
+int print_string(va_list argptr);
+int print_rot13(va_list argptr);
 
-int print_rot13(va_list );
-
-
+/*Print unsigned int*/
+/* int print_unsigned(va_list argptr, *func_mod);*/
 
 /* _putchar.c prototype */
 int _putchar(char c);
